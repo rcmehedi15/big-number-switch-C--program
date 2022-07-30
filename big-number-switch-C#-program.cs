62d22942f10bf7d7222433c9a0456b9e85edfc85
Md Mehedi Hasan <87538600+rcmehedi15@users.noreply.github.com>
2022-07-30 14:02:56 +0600

    big number find switech statement use  c# program

---
+++ big-number-switch-C#-program.cs
@@ -0,0 +1,27 @@
+
+using System;
+namespace hasan
+{
+    class program
+    {
+        public static void Main(string[]args)
+        {
+            string ch = Convert.ToString(Console.ReadLine());
+            switch (ch)
+            {
+                case "ab":
+                    Console.WriteLine("the number is ab");
+                    break;
+                case "ad":
+                    Console.WriteLine("the number is ad");
+                    break;
+                case "add":
+                    Console.WriteLine("the number is add");
+                    break;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
